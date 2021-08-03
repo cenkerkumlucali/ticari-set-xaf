9d2d443bdcd4974e5e311dfbf37bfef69a0d1783
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-08-03 17:14:35 +0300

    Update

--- TicariSet.Module/BusinessObjects/MasrafGrubu.cs
+++ TicariSet.Module/BusinessObjects/MasrafGrubu.cs
@@ -71,19 +71,5 @@ namespace TicariSet.Module.BusinessObjects
 
             base.OnSaving();
         }
-        //private string _PersistentProperty;
-        //[XafDisplayName("My display name"), ToolTip("My hint message")]
-        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
-        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
-        //public string PersistentProperty {
-        //    get { return _PersistentProperty; }
-        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
-        //}
-
-        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
-        //public void ActionMethod() {
-        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
-        //    this.PersistentProperty = "Paid";
-        //}
     }
 }
