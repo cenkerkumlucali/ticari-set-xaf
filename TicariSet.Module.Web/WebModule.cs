b97d46e3a3e273bf5f7a1c125c0c1cf4687dfb2e
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-10-12 13:27:18 +0300

    updated

--- TicariSet.Module.Web/WebModule.cs
+++ TicariSet.Module.Web/WebModule.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
 using System.ComponentModel;
 using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.DC;
 using System.Collections.Generic;
-using DevExpress.ExpressApp.Model;
-using DevExpress.ExpressApp.Editors;
-using DevExpress.ExpressApp.Actions;
 using DevExpress.ExpressApp.Updating;
-using DevExpress.ExpressApp.Model.Core;
-using DevExpress.ExpressApp.Model.DomainLogics;
-using DevExpress.ExpressApp.Model.NodeGenerators;
 using DevExpress.Persistent.BaseImpl;
 
 namespace TicariSet.Module.Web {
