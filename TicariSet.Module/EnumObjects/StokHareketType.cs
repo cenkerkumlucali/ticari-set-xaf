5241b45fc8fb2853ed4ccd8d9bdc2f074fc91c92
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-10-12 13:26:56 +0300

    updated

--- TicariSet.Module/EnumObjects/StokHareketType.cs
+++ TicariSet.Module/EnumObjects/StokHareketType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicariSet.Module.EnumObjects
+﻿namespace TicariSet.Module.EnumObjects
 {
     public enum StokHareketType
     {
