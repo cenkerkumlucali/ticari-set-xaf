9a776d53262de252e0c0bc5ea49171e039721d26
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-10-12 13:27:08 +0300

    updated

--- TicariSet.Module/Module.cs
+++ TicariSet.Module/Module.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Text;
-using System.Linq;
 using DevExpress.ExpressApp;
-using System.ComponentModel;
 using DevExpress.ExpressApp.DC;
 using System.Collections.Generic;
-using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
-using DevExpress.Persistent.BaseImpl.PermissionPolicy;
-using DevExpress.ExpressApp.Model;
-using DevExpress.ExpressApp.Actions;
-using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Updating;
-using DevExpress.ExpressApp.Model.Core;
-using DevExpress.ExpressApp.Model.DomainLogics;
-using DevExpress.ExpressApp.Model.NodeGenerators;
 using DevExpress.ExpressApp.Xpo;
 
 namespace TicariSet.Module {
