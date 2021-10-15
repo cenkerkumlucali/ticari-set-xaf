237738155ab0eb43bfb268c43f07a0f6da5a1f72
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-10-15 12:24:39 +0300

    update

---
+++ TicariSet.Module/EnumObjects/Cinsiyet.cs
@@ -0,0 +1,8 @@
+﻿namespace TicariSet.Module.EnumObjects
+{
+    public enum Cinsiyet
+    {
+        Erkek=0,
+        Bayan=1
+    }
+}
---
+++ TicariSet.Module/EnumObjects/IletisimTipleri.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.ExpressApp.DC;
+
+namespace TicariSet.Module.EnumObjects
+{
+    public enum IletisimTipleri
+    {
+        [XafDisplayName("Telefon-1")]
+        Cep1 = 0,
+        [XafDisplayName("Telefon-2")]
+        Cep2 = 1,
+        [XafDisplayName("Telefon-3")]
+        Cep3 = 2,
+        [XafDisplayName("Gsm")]
+        Gsm = 3,
+        [XafDisplayName("E-posta")]
+        Eposta = 4,
+        Fax = 5,
+        [XafDisplayName("Web Adresi")]
+        WebAdresi = 6
+    }
+}
