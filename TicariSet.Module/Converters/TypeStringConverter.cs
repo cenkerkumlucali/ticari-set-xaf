0b97075c55b69d4c002433b849fd23167e6d7d6b
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-10-18 15:58:08 +0300

    update

---
+++ TicariSet.Module/Converters/TypeStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo.Metadata;
+
+namespace TicariSet.Module.BusinessObjects
+{
+    public class TypeStringConverter : ValueConverter
+    {
+
+        #region Overrides
+
+        public override object ConvertFromStorageType(object stringObjectType)
+        {
+            return ReflectionHelper.FindType((string)stringObjectType);
+        }
+
+        public override object ConvertToStorageType(object objectType)
+        {
+            if (objectType == null)
+            {
+                return null;
+            }
+
+            return ReflectionHelper.GetType(objectType.ToString()).Name;
+        }
+
+        public override Type StorageType => typeof(string);
+
+        #endregion
+    }
+}
