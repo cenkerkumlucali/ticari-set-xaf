7a9b7c7507866e19a7a139e0a5f8d0a0d7b17e75
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-10-20 13:53:18 +0300

    update

---
+++ TicariSet.Module.Win/Controllers/WinAlternatingRowsController.Designer.cs
@@ -0,0 +1,45 @@
+﻿
+using TicariSet.Module.BusinessObjects;
+
+namespace TicariSet.Module.Controllers
+{
+    partial class WinAlternatingRowsController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // FislerViewController
+            // 
+            this.TargetObjectType = typeof(Fisler);
+            this.TargetViewNesting = DevExpress.ExpressApp.Nesting.Nested;
+            this.TargetViewType = DevExpress.ExpressApp.ViewType.ListView;
+
+        }
+
+        #endregion
+    }
+}
---
+++ TicariSet.Module.Win/Controllers/WinAlternatingRowsController.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.XtraGrid.Views.Grid;
+using TicariSet.Module.BusinessObjects;
+
+namespace TicariSet.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class WinAlternatingRowsController : ViewController
+    {
+        public WinAlternatingRowsController()
+        {
+                InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+          
+            // Perform various tasks depending on the target View.
+        }
+        private void WinAlternatingRowsController_ViewControlsCreated(object sender, EventArgs e)
+        {
+            GridListEditor listEditor = ((ListView)View).Editor as GridListEditor;
+            if (listEditor != null)
+            {
+                GridView gridView = listEditor.GridView;
+                gridView.OptionsView.EnableAppearanceOddRow = true;
+                gridView.Appearance.OddRow.BackColor = Color.FromArgb(244, 244, 244);
+            }
+        }
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
