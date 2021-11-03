91a273906f17f0395f466a28e7e8a6f38a3f35a7
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-11-03 11:38:02 +0300

    update

---
+++ TicariSet.Module/BusinessObjects/EvrakBeden.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+
+namespace TicariSet.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    [DefaultProperty("Toplam")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class EvrakBeden : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public EvrakBeden(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        int miktar;
+        string beden;
+        EvrakSatir evrakSatirId;
+        int toplam;
+
+        //rowcellclick
+        //focusedcolumnchanged
+
+        public EvrakSatir EvrakSatirId
+        {
+            get => evrakSatirId;
+            set => SetPropertyValue(nameof(EvrakSatirId), ref evrakSatirId, value);
+        }
+
+        [Size(16)]
+        public string Beden
+        {
+            get => beden;
+            set => SetPropertyValue(nameof(Beden), ref beden, value);
+        }
+        
+        public int Miktar
+        {
+            get => miktar;
+            set
+            {
+                if (SetPropertyValue(nameof(Miktar), ref miktar, value)) ;
+            }
+        }
+
+
+    }
+}
---
+++ TicariSet.Module/BusinessObjects/EvrakSatir.cs
@@ -0,0 +1,76 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace TicariSet.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    [DefaultProperty("Kod")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class EvrakSatir : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public EvrakSatir(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        int miktar;
+        EvrakBeden evrakBeden;
+        Evrak evrakId;
+        string kod;
+
+        [Size(64)]
+        public string Kod
+        {
+            get => kod;
+            set => SetPropertyValue(nameof(Kod), ref kod, value);
+        }
+
+        [Association("Evrak-EvrakSatir")]
+        public Evrak EvrakId
+        {
+            get => evrakId;
+            set => SetPropertyValue(nameof(EvrakId), ref evrakId, value);
+        }
+        
+        public int Miktar
+        {
+            get => miktar;
+            set
+            {
+                SetPropertyValue(nameof(Miktar), ref miktar, value);
+
+            }
+        }
+        [XafDisplayName("Beden")]
+        public EvrakBeden BedenId
+        {
+            get => evrakBeden;
+            set => SetPropertyValue(nameof(BedenId), ref evrakBeden, value);
+        }
+
+        public static void HesaplaMiktar(EvrakSatir evrakSatir,int toplam)
+        {
+            evrakSatir.Miktar = toplam;
+        }
+    }
+}
