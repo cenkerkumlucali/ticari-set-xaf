718507b0c0b9696f059ddf42bcf9337c793f937d
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-11-04 08:06:45 +0300

    update

--- TicariSet.Module/BusinessObjects/EvrakBeden.cs
+++ TicariSet.Module/BusinessObjects/EvrakBeden.cs
@@ -1,18 +1,7 @@
-﻿using DevExpress.Data.Filtering;
-using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.DC;
-using DevExpress.ExpressApp.Model;
-using DevExpress.Persistent.Base;
+﻿using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
-using DevExpress.Persistent.Validation;
 using DevExpress.Xpo;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Net.Mime;
-using System.Text;
 
 namespace TicariSet.Module.BusinessObjects
 {
