d1ecf070040344c02e58b3f86c6499c8be2216ee
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-11-04 08:06:37 +0300

    update

--- TicariSet.Module/BusinessObjects/Evrak.cs
+++ TicariSet.Module/BusinessObjects/Evrak.cs
@@ -1,21 +1,12 @@
-﻿using DevExpress.Data.Filtering;
-using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.DC;
-using DevExpress.ExpressApp.Model;
-using DevExpress.Persistent.Base;
+﻿using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
-using DevExpress.Persistent.Validation;
 using DevExpress.Xpo;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 
 namespace TicariSet.Module.BusinessObjects
 {
     [DefaultClassOptions]
-    //[ImageName("BO_Contact")]
+    [ImageName("BO_Contract")]
     [DefaultProperty("EvrakNo")]
     //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
     //[Persistent("DatabaseTableName")]
