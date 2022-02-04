b3ae214eb1023dfa2dd4c4f87ce37cc8e0368984
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-02-04 12:39:19 +0300

    update

--- TicariSet.Module/Module.cs
+++ TicariSet.Module/Module.cs
@@ -2,10 +2,12 @@
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
 using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.ExpressApp.Updating;
 using DevExpress.ExpressApp.Xpo;
 using SmartAssembly.Attributes;
+using TicariSet.Module.Interfaces;
 
 namespace TicariSet.Module {
     // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
@@ -13,6 +15,14 @@ namespace TicariSet.Module {
         public TicariSetModule() {
             InitializeComponent();
 			BaseObject.OidInitializationMode = OidInitializationMode.AfterConstruction;
+            //ApplicationModelManager.RaiseExceptionWhenRequiredPropertyIsNull = false;
+        }
+        public override void ExtendModelInterfaces(ModelInterfaceExtenders extenders)
+        {
+            base.ExtendModelInterfaces(extenders);
+            extenders.Add<IModelListView, IModelListViewEx>();
+            extenders.Add<IModelColumn, IModelColumnEx>();
+            extenders.Add<IModelOptions, IModelOptionsEx>();
         }
         public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
             ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
